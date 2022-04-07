a108801f79f080907daea377e4d0bc8ce24b090e
quinnest <eolaosho79@gmail.com>
2022-04-07 13:35:01 +0100

    my first push

---
+++ src/components/navbar.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+//stateless Functional Componenet
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        NavBar {" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+
+        </span >
+
+      </a >
+    </nav >);
+};
+
+export default NavBar;
+
+
